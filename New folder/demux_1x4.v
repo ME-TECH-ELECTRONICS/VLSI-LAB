bed1215f72fd5166e84beb415b8e01685758d2ed
METECH-ALT <melvinrijohnt@gmail.com>
2024-08-23 12:59:13 +0530

    v1.0

--- New folder/demux_1x4.v
+++ New folder/demux_1x4.v
@@ -1,26 +1,25 @@
 
-module demux_2x1(d,s,y0,y1);
+module demux_1x2(d,s,y0,y1);
 	input d,s;
 	output y0,y1;
 	assign y0 = d&(~s);
 	assign y1 = d&s; 
 endmodule 
 
-module demux_1x4(d,s0, s1,y0,y1);
-	input s,d;
+module demux_1x4(d,s0,s1,y0,y1,y2,y3);
+	input s0,s1,d;
 	output y0,y1,y2,y3;
-	wire  
-	demux_1x4 dx0(d,s0,t1,t2);
-	demux_1x4 dx1(d,s1,y0,y1);
-	demux_1x4 dx2(d,s1,y2,y3);
+	wire t1,t2; 
+	demux_1x2 d0(d,s0,t1,t2);
+	demux_1x2 d1(d,s1,y0,y1);
+	demux_1x2 d2(d,s1,y2,y3);
+endmodule
 //TestBench
 
 module demux_1x4_tb;
 	reg d,s0,s1;
 	wire y0,y1,y2,y3;
-	
-	demux_1x4 dut(d,s,y0,y1);
-	
+	demux_1x4 dut(d,s0,s1,y0,y1,y2,y3);
 	initial begin
 		s0=0; s1=0; y0=1; y1=0; y2=0; y3=1; #10;
 		s0=0; s1=0; y0=0; y1=1; y2=1; y3=1; #10;
