868ece1e3940810d8f05700875413d7748db45ea
ME TECH <69456138+ME-TECH-ELECTRONICS@users.noreply.github.com>
2024-08-28 21:17:52 +0530

    update mux_16x1.v

--- gate-level/mux_16x1.v
+++ gate-level/mux_16x1.v
@@ -2,8 +2,10 @@ module mux_4x1(y,s0,s1,d0,d1,d2,d3);
     input d0,d1,d2,d3,s0,s1;
     output y;
     wire s_0,s_1,t0,t1,t2,t3;
-    and();
-    and();
-    and();
-    and();
+    not(s_0,s0);
+    not(s_1,s1);
+    and(t0,s_0,s_1,d0);
+    and(t1,s_0,s1,d1);
+    and(t2,s0,s_1,d2);
+    and(t3,s0,s1,d3);
 endmodule
