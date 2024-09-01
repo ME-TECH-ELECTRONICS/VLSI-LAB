3e11a8c102a27561fab33c1f76c67d5b126124ee
ME TECH <69456138+ME-TECH-ELECTRONICS@users.noreply.github.com>
2024-09-01 10:46:00 +0530

    update dff.v

--- behavioral/dff.v
+++ behavioral/dff.v
@@ -1,19 +1,18 @@
 module dff (
     input wire D,
-    input wire clk 
+    input wire clk
     input wire rst,
     output reg Q;
 );
+    always @(posedge clk or posedge rst) begin
     
+    end
 endmodule
 
 module tb();
-    clk=0;
+    clk = 0;
     always #5 clk=~clk;
     initial begin
-         
-        
-            
+
     end
-    
 endmodule
