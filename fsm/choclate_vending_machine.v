bfbd3ed2d3357112431049f16e999161bde77680
ME TECH <69456138+ME-TECH-ELECTRONICS@users.noreply.github.com>
2024-09-30 07:15:48 +0530

    update choclate_vending_machine.v

--- fsm/choclate_vending_machine.v
+++ fsm/choclate_vending_machine.v
@@ -1,7 +1,7 @@
 
 module vending_machine(
     input clk,
-    input reset,
+    input rst,
     input [1:0] coin, 
     output reg [2:0] chocolate 
 );
@@ -9,44 +9,44 @@ module vending_machine(
 // State Encoding
 parameter IDLE = 2'b00, FIVE = 2'b01, TEN = 2'b10, TWENTY = 2'b11;
 
-reg [1:0] current_state, next_state;
+reg [1:0] PS, NS;
 
 // State Transition
-always @(posedge clk or posedge reset) begin
-    if (reset)
-        current_state <= IDLE;
+always @(posedge clk) begin
+    if (rst)
+        PS <= IDLE;
     else
-        current_state <= next_state;
+        PS <= NS;
 end
 
 // Next State Logic and Output Logic
 always @(*) begin
-    next_state = current_state; // Default to stay in current state
+    NS = PS; // Default to stay in current state
     chocolate = 3'b000; // Default to no chocolate
     
-    case (current_state)
+    case (PS)
         IDLE: begin
             if (coin == 2'b01)
-                next_state = FIVE;
+                NS = FIVE;
             else if (coin == 2'b10)
-                next_state = TEN;
+                NS = TEN;
             else if (coin == 2'b11)
-                next_state = TWENTY;
+                NS = TWENTY;
         end
         
         FIVE: begin
             chocolate = 3'b001; // Dispense chocolate A
-            next_state = IDLE;  // Return to IDLE after dispensing
+            NS = IDLE;  // Return to IDLE after dispensing
         end
         
         TEN: begin
             chocolate = 3'b010; // Dispense chocolate B
-            next_state = IDLE;  // Return to IDLE after dispensing
+            NS = IDLE;  // Return to IDLE after dispensing
         end
         
         TWENTY: begin
             chocolate = 3'b100; // Dispense chocolate C
-            next_state = IDLE;  // Return to IDLE after dispensing
+            NS = IDLE;  // Return to IDLE after dispensing
         end
     endcase
 end
@@ -57,14 +57,14 @@ module choco_tb();
 
     // Testbench signals
     reg clk;
-    reg reset;
+    reg rst;
     reg [1:0] coin;
     wire [2:0] chocolate;
 
     // Instantiate the vending machine module
     vending_machine uut (
         .clk(clk),
-        .reset(reset),
+        .rst(rst),
         .coin(coin),
         .chocolate(chocolate)
     );
@@ -78,12 +78,12 @@ module choco_tb();
     // Test sequence
     initial begin
         // Initialize signals
-        reset = 1;
+        rst = 1;
         coin = 2'b00;
         #10;
         
-        // Release reset
-        reset = 0;
+        // Release rst
+        rst = 0;
 
         // Test case 1: Insert 5
         #10 coin = 2'b01;  // Insert 5 units
