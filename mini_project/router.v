02ee53d7292048c1d3499a2626ebda60bc5e80f2
ME TECH <melvin.rt.123@gmail.com>
2024-10-12 13:07:17 +0530

    v1.0

--- mini_project/router.v
+++ mini_project/router.v
@@ -1,13 +1,13 @@
-/****************************************************/
-//      AUTHOR: METECH
-//      FILE_NAME: router.v
-//      DESCRIPTION: Top level module of a 1x3 router
-//      DATE: 08/10/2024
-/****************************************************/
+/********************************************************/
+/*      AUTHOR: METECH                                  */
+/*      FILE_NAME: router.v                             */
+/*      DESCRIPTION: Top level module of a 1x3 router   */
+/*      DATE: 08/10/2024                                */
+/********************************************************/
 
 module router (
-    input clk,                     // Clock input
-    input rst,                     // Reset input
+    input clk,                    // Clock input
+    input rst,                    // Reset input
     input [7:0] d_in,             // Data input (8 bits)
     input pkt_valid,              // Packet validity signal
     input rd_en_0,                // Read enable for FIFO 0
@@ -20,7 +20,7 @@ module router (
     output busy,                  // Busy signal indicating processing
     output [7:0] dout_0,          // Data output from FIFO 0
     output [7:0] dout_1,          // Data output from FIFO 1
-    output [7:0] dout_2            // Data output from FIFO 2
+    output [7:0] dout_2           // Data output from FIFO 2
 );
 
     // Internal wire declarations for FIFO control signals and state management
@@ -74,15 +74,15 @@ endmodule
 
 module router_tb(); // Testbench for the router
 
-    reg clk=0;               // Clock signal
-    reg rst;                   // Reset signal
-    reg pkt_valid;            // Packet validity signal
-    reg rd_en_0, rd_en_1, rd_en_2; // Read enable signals for FIFOs
-    reg [7:0] din;            // Data input to router
-    wire vld_out_0, vld_out_1, vld_out_2; // Valid outputs from FIFOs
-    wire err, busy;           // Error and busy signals
-    wire [7:0] dout_0, dout_1, dout_2; // Data outputs from FIFOs
-    integer i;                // Loop variable for testing
+    reg clk=0;                              // Clock signal
+    reg rst;                                // Reset signal
+    reg pkt_valid;                          // Packet validity signal
+    reg rd_en_0, rd_en_1, rd_en_2;          // Read enable signals for FIFOs
+    reg [7:0] din;                          // Data input to router
+    wire vld_out_0, vld_out_1, vld_out_2;   // Valid outputs from FIFOs
+    wire err, busy;                         // Error and busy signals
+    wire [7:0] dout_0, dout_1, dout_2;      // Data outputs from FIFOs
+    integer i;                              // Loop variable for testing
     router dut(clk, rst, din, pkt_valid, rd_en_0, rd_en_1, rd_en_2, vld_out_0, vld_out_1, vld_out_2, err, busy, dout_0, dout_1, dout_2);
     
     // Clock generation
@@ -90,52 +90,58 @@ module router_tb(); // Testbench for the router
     
     // Task for generating a payload of specified length
     task payload_XB;
-        input [6:0] len;        // Length of the payload
-        input read;             // Read enable flag
-        input [1:0] addr;       // Address to write/read from FIFO
-        input [7:0] ext_parity; // External parity for the payload
-        reg [7:0] header, data; // Header and data variables
-        reg [7:0] parity;   // Parity accumulator
+        input [6:0] len;            // Length of the payload
+        input read;                 // Read enable flag
+        input simRead;              // Simulation read enable flag
+        input [1:0] addr;           // Address to write/read from FIFO
+        input [7:0] ext_parity;     // External parity for the payload
+        reg [7:0] header, data;     // Header and data variables
+        reg [7:0] parity;           // Parity accumulator
         begin
             parity = 0;
             wait(!busy) begin 
-                @(negedge clk); // Wait for negative edge of clock
-                pkt_valid = 1;  // Indicate packet is valid
-                header = {len, addr}; // Create header with length and address
-                din = header;   // Load header into data input
-                parity = parity ^ din; // Calculate parity
+                @(negedge clk);         // Wait for negative edge of clock
+                pkt_valid = 1;          // Indicate packet is valid
+                header = {len, addr};   // Create header with length and address
+                din = header;           // Load header into data input
+                parity = parity ^ din;  // Calculate parity
             end
-            @(negedge clk); // Wait for negative edge of clock
+            @(negedge clk);             // Wait for negative edge of clock
             for (i = 0; i < len; i = i + 1) begin
                 wait(!busy) begin 
-                    @(negedge clk); // Wait for negative edge of clock
-                    data = {$random} % 256; // Generate random data byte
-                    din = data;   // Load data into data input
-                    parity = parity ^ din; // Update parity
+                    @(negedge clk);             // Wait for negative edge of clock
+                    data = {$random} % 256;     // Generate random data byte
+                    din = data;                 // Load data into data input
+                    parity = parity ^ din;      // Update parity
+                end
+                if(read && simRead && i == 5) begin
+                    rd_en_0 = (addr == 0);      // Set read enable for FIFO 0
+                    rd_en_1 = (addr == 1);      // Set read enable for FIFO 1
+                    rd_en_2 = (addr == 2);      // Set read enable for FIFO 2
                 end
             end
             
             wait(!busy) begin 
-                @(negedge clk); // Wait for negative edge of clock
-                pkt_valid = 0;  // Indicate packet is no longer valid
-                din = (ext_parity == 0) ? parity : ext_parity; // Load parity or external parity
+                @(negedge clk);                                 // Wait for negative edge of clock
+                pkt_valid = 0;                                  // Indicate packet is no longer valid
+                din = (ext_parity == 0) ? parity : ext_parity;  // Load parity or external parity
             end
             
             if (read) begin
-                @(negedge clk); // Wait for negative edge of clock
-                @(negedge clk); // Wait for negative edge of clock
+                @(negedge clk);     // Wait for negative edge of clock
+                @(negedge clk);     // Wait for negative edge of clock
                 
                 if(addr == 0) begin
-                    rd_en_0 = 1; // Set read enable for FIFO 0
-                    wait(dut.FIFO_0.empty); // Wait until FIFO 0 is empty
+                    rd_en_0 = simRead ? rd_en_0 : 1;        // Set read enable for FIFO 0
+                    wait(dut.FIFO_0.empty);                 // Wait until FIFO 0 is empty
                     rd_en_0 = 0;
                 end else if(addr == 1) begin
-                    rd_en_1 = 1; // Set read enable for FIFO 1
-                    wait(dut.FIFO_1.empty); // Wait until FIFO 1 is empty
+                    rd_en_1 = simRead ? rd_en_1 : 1;        // Set read enable for FIFO 1
+                    wait(dut.FIFO_1.empty);                 // Wait until FIFO 1 is empty
                     rd_en_1 = 0;
                 end else if(addr == 2) begin
-                    rd_en_2 = 1; // Set read enable for FIFO 2
-                    wait(dut.FIFO_2.empty); // Wait until FIFO 2 is empty
+                    rd_en_2 = simRead ? rd_en_2 : 1;        // Set read enable for FIFO 2
+                    wait(dut.FIFO_2.empty);                 // Wait until FIFO 2 is empty
                     rd_en_2 = 0;
                 end
             end
@@ -154,13 +160,14 @@ module router_tb(); // Testbench for the router
         #10 rst = 1; // De assert reset
         
         // Test cases with different payload lengths and read addresses
-        // #10 payload_XB(6'd8, 1, 2'b0, 0); // Payload Length = 8Bytes, Address = 0, sequential Read.
-        // #10; rst = 0; #10; rst = 1;
-        #10 payload_XB(6'd13, 1, 2'b0, 0); // Payload Length = 16Bytes, Address = 1, simultaneous Read.
-        // #70; rst = 0; #10; rst = 1;
-        // #10 payload_XB(6'd16, 0, 2'b1, 0); // Payload Length = 16Bytes, Address = 1, Reading Disabled.
-        // #70; rst = 0; #10; rst = 1;
-        // #10 payload_XB(6'd16, 1, 2'b1, 8'h28); // Payload Length = 16Bytes, Address = 2, Corrupted packet.
+        #20 payload_XB(6'd8, 1, 0, 2'b0, 0); // Payload Length = 8Bytes, Address = 0, sequential Write & Read.
+        #30; rst = 0; #10; rst = 1;
+        #20 payload_XB(6'd20, 1, 1, 2'b0, 0); // Payload Length = 20Bytes, Address = 1, simultaneous Write & Read.
+        #30; rst = 0; #10; rst = 1;
+        #20 payload_XB(6'd16, 0, 0,2'b1, 0); // Payload Length = 16Bytes, Address = 1, Reading Disabled.
+        #30; rst = 0; #10; rst = 1;
+        #20 payload_XB(6'd14, 1, 0, 2'b10, 8'h25); // Payload Length = 16Bytes, Address = 2, Corrupted packet. Squential Write & Read.
     end
      
 endmodule
+
