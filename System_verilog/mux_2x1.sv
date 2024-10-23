e887ef46dcab4b9bc95d2daf69ec9179c80c3de9
ME TECH <melvin.rt.123@gmail.com>
2024-10-23 18:38:38 +0530

    v1.0

--- System_verilog/mux_2x1.sv
+++ System_verilog/mux_2x1.sv
@@ -11,16 +11,16 @@ module mux_2x1_tb ();
     logic a,b,s,y;
     mux_2x1 dut(a,b,s,y);
     initial begin
-        $monitor("a=%0d b=%0d s=%0d y=%0d",a,b,s,y);
-        a=0; b=0; s=0;
-        a=0; b=1; s=0;
-        a=1; b=0; s=0;
-        a=1; b=1; s=0;
-        a=0; b=0; s=1;
-        a=0; b=1; s=1;
-        a=1; b=0; s=1;
-        a=1; b=1; s=1;
         $dumpfile("out.vcd");
         $dumpvars();
+        $monitor("a=%0d b=%0d s=%0d y=%0d",a,b,s,y);
+        a=0; b=0; s=0; #10;
+        a=0; b=1; s=0; #10;
+        a=1; b=0; s=0; #10;
+        a=1; b=1; s=0; #10;
+        a=0; b=0; s=1; #10;
+        a=0; b=1; s=1; #10;
+        a=1; b=0; s=1; #10;
+        a=1; b=1; s=1; #10;
     end
 endmodule
