ab59b1073036b8837cf8ee2bf1b5d10cf88efd14
ME TECH <69456138+ME-TECH-ELECTRONICS@users.noreply.github.com>
2024-10-23 22:18:08 +0530

    update array_types.sv

---
+++ System_verilog/array_types.sv
@@ -0,0 +1,13 @@
+module array_types();
+    int arr[3] = {20,40,34};
+    string arr[3] {"Hello","World","!"};
+    string arr3[];
+    int arr4[string];
+    
+    
+    endtask
+    initial begin
+        
+        
+    end
+endmodule
