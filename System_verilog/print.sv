4093b8cc951c0b0ad8cdf6da3357d04846efbe50
ME TECH <melvin.rt.123@gmail.com>
2024-10-24 18:16:58 +0530

    v1.0

--- System_verilog/print.sv
+++ System_verilog/print.sv
@@ -1,6 +1,52 @@
 module printMsg ();
-   string msg = "Hello world!";
     initial begin
-        $display("Msg: %s", msg);
+        a = 0;
+        b=1;
+        clk = 1;
+        forever begin
+            clk = ~clk;
+            #10;
+        end
     end
+
+    initial begin
+        #10 a =1;
+        b=0;
+        @(posedge clk)
+        #2 a= #5 b;
+
+b=#3 b;
+
+end
+
+initial begin
+
+#70;
+
+@(posedge elk)
+
+@(posedge clk)
+
+@(negedge clk)
+
+a=1'hx;
+
+@(negedge clk)
+B=1'hx;
+end
+
+initial begin
+
+@(posedge clk)
+
+@(posedge clk)
+
+@(posedge clk)
+
+a=1;
+
+@(negedge clk)
+
+b=0;
+end
 endmodule
