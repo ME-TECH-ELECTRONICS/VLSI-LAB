51f7a6961c834fe9f6e58caa3a7ccabd4630c498
ME TECH <69456138+ME-TECH-ELECTRONICS@users.noreply.github.com>
2024-10-24 08:16:49 +0530

    update process_tasks.sv

--- System_verilog/process_tasks.sv
+++ System_verilog/process_tasks.sv
@@ -22,5 +22,20 @@ module process_task();
         a = 37; b = 8; c = 66; #10;
         $display("/***** Initial Values ****/");
         $display("a = %0d b = %0d c = %0d", a,b,c);
+        fork
+            t1(a,b);
+            t2(a,b);
+        join
+        $display("Sum & Differnce: %0d", f1(a,b,c));
+        fork
+            t1(a,b);
+            t2(a,b);
+        join_any
+        $display("Sum & Differnce: %0d", f1(a,b,c));
+        fork
+            t1(a,b);
+            t2(a,b);
+        join_none
+        $display("Sum & Differnce: %0d", f1(a,b,c));
     end
 endmodule
