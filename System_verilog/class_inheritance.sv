75b154bfe6223541bbf1930510bd660c4c3957e0
ME TECH <69456138+ME-TECH-ELECTRONICS@users.noreply.github.com>
2024-11-05 20:58:28 +0530

    update class_inheritance.sv

--- System_verilog/class_inheritance.sv
+++ System_verilog/class_inheritance.sv
@@ -1,9 +1,6 @@
-// Base class
 class Shape;
-    // Properties
     string name;
 
-    // Constructor for Shape class
     function new(string name);
         this.name = name;
     endfunction
@@ -26,32 +23,26 @@ class Circle extends Shape;
     endfunction
 endclass
 
-// Derived class: Rectangle
 class Rectangle extends Shape;
     real length, width;
 
-    // Constructor for Rectangle class
     function new(real length, real width);
         super.new("Rectangle");
         this.length = length;
         this.width = width;
     endfunction
 
-    // Override the calculate_area method for Rectangle
     function real calculate_area();
         return length * width;
     endfunction
 endclass
 
-// Test Program
 module test;
     initial begin
-        // Create an instance of Circle
         Circle c = new(5.0);
         c.display();
         $display("Area of %s: %0.2f", c.name, c.calculate_area());
 
-        // Create an instance of Rectangle
         Rectangle r = new(4.0, 7.0);
         r.display();
         $display("Area of %s: %0.2f", r.name, r.calculate_area());
