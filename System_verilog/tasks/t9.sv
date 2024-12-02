568cd12a3ae14d373cb49df0afb69f859720c72d
ME TECH <69456138+ME-TECH-ELECTRONICS@users.noreply.github.com>
2024-12-02 11:03:15 +0530

    update t9.sv

--- System_verilog/tasks/t9.sv
+++ System_verilog/tasks/t9.sv
@@ -3,7 +3,16 @@ module tb();
     logic[8:0] address;
     
     task myTask(ref int arr[512], ref logic[8:0] addr);
-        for(int i = arr.size(); i >= 0; i= i - 1)
-    
+        for(int i = arr.size(); i >= 0; i= i - 1) begin
+            print_int(arr[i]);
+        end
     endtask
+    
+    function void print_int(int val);
+        $display("%0t: Value = %0d", $time, val);
+    endfunction
+    
+    initial begin
+        
+    end
 endmodule
