6504bbefc0c4c291df566f15b9715c26705ba395
ME TECH <69456138+ME-TECH-ELECTRONICS@users.noreply.github.com>
2024-12-12 22:29:37 +0530

    update transaction.sv

--- System_verilog/testbench/transaction.sv
+++ System_verilog/testbench/transaction.sv
@@ -1,1 +1,8 @@
-class 
+class Packet;
+    rand a, b, c;
+    bit sum, carry;
+    
+    function void print(string comp);
+        $display("[%0tps] %0s: a=%0d, b=%0d, c=%0d, sum = %0d, carry = %0d");
+    endfunction
+endclass
