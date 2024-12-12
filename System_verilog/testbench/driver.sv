336d69e8367b7eec03712f751026a5135c39888e
ME TECH <69456138+ME-TECH-ELECTRONICS@users.noreply.github.com>
2024-12-12 22:44:20 +0530

    update driver.sv

--- System_verilog/testbench/driver.sv
+++ System_verilog/testbench/driver.sv
@@ -2,10 +2,26 @@ class Driver;
     event drv_done;
     mailbox mbx;
     virtual adder_intf vif;
+    Packet item;
     
-    function new(event drv_done, mailbox mbx, virtual adder_intf vif);
+    function new(event drv_done, mailbox mbx, virtual adder_intf vif, Packet item);
         this.drv_done = drv_done;
         this.mbx = mbx;
         this.vif = vif;
+        this.item = item;
     endfunction
+    
+    task run();
+        $display("[%0tps] Driver: Starting...", $time);
+        forever begin
+            Packet item;
+            $display("[%0tps] Driver: Waiting for input...", $time);
+            mbx.get(item);
+            vif.a <= item.a;
+            vif.b <= item.b;
+            vif.c <= item.c;
+            ->drv_done;
+        end
+        
+    endtask
 endclass
