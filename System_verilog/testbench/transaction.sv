ec2d599c666ee4e9f27bcf25f2c59d1860fa497d
ME TECH <69456138+ME-TECH-ELECTRONICS@users.noreply.github.com>
2024-12-16 22:05:46 +0530

    update transaction.sv

--- System_verilog/testbench/transaction.sv
+++ System_verilog/testbench/transaction.sv
@@ -1,10 +1,16 @@
 class Packet;
-    rand bit[7:0] a, b,;
-    rand bit cin;
+    rand bit[7:0] a, b;
     bit[7:0] sum; 
     bit carry;
     
     function void print(string comp);
         $display("[%0tps] %0s: a=%0d, b=%0d, c=%0d, sum = %0d, carry = %0d", $time, comp, a, b, c, sum, carry);
+    endfunction
+    
+    function void copy(Packet tmp);
+        this.a = tmp.a;
+        this.b = tmp.b;
+        this.sum = tmp.sum;
+        this.carry = tmp.carry;
     endfunction
 endclass
