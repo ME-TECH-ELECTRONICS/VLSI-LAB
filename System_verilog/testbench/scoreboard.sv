05c43bcd89f82aa321b98f30ea31751bde4e06e8
ME TECH <69456138+ME-TECH-ELECTRONICS@users.noreply.github.com>
2024-12-16 21:57:02 +0530

    update scoreboard.sv

--- System_verilog/testbench/scoreboard.sv
+++ System_verilog/testbench/scoreboard.sv
@@ -12,12 +12,17 @@ class Scoreboard;
             ref_item.copy(item);
             
             {ref_item.carry, ref_item.sum} = ref_item.a + ref_item.b;
+            
             if(ref_item.carry != item.carry) begin 
                 $display("[%0tps] Scoreboard: Error:- Carry mismatch ref_item.carry = %0d, item.carry = %0d", $time, ref_item.carry, item.carry);
+            end else begin 
+                $display("[%0tps] Scoreboard:  Pass:- Carry match ref_item.carry = %0d, item.carry = %0d", $time, ref_item.carry, item.carry);
             end
-            else begin()
+            
             if(ref_item.sum != item.sum) begin 
                 $display("[%0tps] Scoreboard: Error:- Sum mismatch ref_item.sum = %0d, item.sum = %0d", $time, ref_item.sum, item.sum);
+            end else begin 
+                $display("[%0tps] Scoreboard: Pass:- Sum match ref_item.sum = %0d, item.sum = %0d", $time, ref_item.sum, item.sum);
             end
         end
     endtask
