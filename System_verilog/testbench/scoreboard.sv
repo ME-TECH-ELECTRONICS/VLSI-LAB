df971db6c71da4391ba89dcdb557712ed2eb84e6
ME TECH <69456138+ME-TECH-ELECTRONICS@users.noreply.github.com>
2024-12-16 21:53:43 +0530

    update scoreboard.sv

--- System_verilog/testbench/scoreboard.sv
+++ System_verilog/testbench/scoreboard.sv
@@ -1,6 +1,24 @@
 class Scoreboard;
-    mailbox scb_mbx;
-    function new();
-        
-    endfunction 
+    mailbox mbx;
+    function new(mailbox mbx);
+        this.mbx = mbx;
+    endfunction
+    
+    task run();
+        forever begin 
+            Packet item, ref_item;
+            mbx.get(item);
+            ref_item = new();
+            ref_item.copy(item);
+            
+            {ref_item.carry, ref_item.sum} = ref_item.a + ref_item.b;
+            if(ref_item.carry != item.carry) begin 
+                $display("[%0tps] Scoreboard: Error:- Carry mismatch ref_item.carry = %0d, item.carry = %0d", $time, ref_item.carry, item.carry);
+            end
+            else begin()
+            if(ref_item.sum != item.sum) begin 
+                $display("[%0tps] Scoreboard: Error:- Sum mismatch ref_item.sum = %0d, item.sum = %0d", $time, ref_item.sum, item.sum);
+            end
+        end
+    endtask
 endclass
