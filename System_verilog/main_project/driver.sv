d9bf5a02ab612767bee73e4329711d6f11d537dd
ME-TECH-ELECTRONICS <melvin.rt.123@gmail.com>
2024-12-23 22:23:52 +0530

    v1.0

--- System_verilog/main_project/driver.sv
+++ System_verilog/main_project/driver.sv
@@ -1,11 +1,13 @@
 class driver;
     mailbox mbx;
     event drv_done;
+    event headerByte;
     virtual router_if vif;
     virtual router_clk clk_vif;
-    function new(mailbox mbx, event drv_done, router_if vif, router_clk clk_vif);
+    function new(mailbox mbx, event drv_done, router_if vif, router_clk clk_vif, event headerByte);
         this.mbx = mbx;
         this.drv_done = drv_done;
+        this.headerByte = headerByte;
         this.vif = vif;
         this.clk_vif = clk_vif;
     endfunction
@@ -13,7 +15,9 @@ class driver;
     task run();
         $display("[%0tps] Driver: Starting...", $time);
         forever begin
+
             Payload pld = new();
+            @(headerByte);
             @(clk_vif.clk);
             $display("[%0tps] Driver: Waiting for input...", $time);
             mbx.get(pld);
--- System_verilog/main_project/generator.sv
+++ System_verilog/main_project/generator.sv
@@ -1,24 +1,26 @@
  class generator;
     mailbox mbx;
     event drv_done;
+    event headerByte;
 
-    function new(mailbox mbx, event drv_done);
+    function new(mailbox mbx, event drv_done, event headerByte);
         this.mbx = mbx;
         this.drv_done = drv_done;
+        this.headerByte = headerByte;
     endfunction
 
     task run();
         Payload pld;
         Header hdr;
-        int i;
         for (int i = 0; i < 5; i++) begin
             pld = new();
             hdr = new();
-            pld.randomize();
-            hdr.randomize();
+            if(!pld.randomize()) $error("Payload randomization failed");
+            if(!hdr.randomize()) $error("Header randomization failed");
             pld.print("generator");
             hdr.print("generator");
             mbx.put(pld, hdr);
+            ->headerByte;
             @(drv_done);
         end
     endtask
