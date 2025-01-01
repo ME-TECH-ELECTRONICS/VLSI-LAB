7decfb01fc0f93f346ea7ccda7c37f8353681f14
ME-TECH-ELECTRONICS <melvin.rt.123@gmail.com>
2025-01-01 08:09:56 +0530

    v1.0

--- System_verilog/main_project/scoreboard.sv
+++ System_verilog/main_project/scoreboard.sv
@@ -13,10 +13,34 @@ class Scoreboard;
     task run();
         Packet pkt;
         mbx.get(pkt);
-        
     endtask
     
     task checkPacket(Packet item);
+        if(item.pkt_valid && item.rst) begin
+            case (pkt.pkt_type)
+                HEADER: begin
+                    header = item.data;
+                    in_stream = new[header[7:0] + 2];
+                    out_stream = new[header[7:0] + 2];
+                    pushData(item);
+                    
+                end
+                PAYLOAD: drive_payload(pkt);
+                PARITY: drive_parity(pkt);
+                default: ;
+            endcase
+            if(item.pkt_type == HEADER) begin
+                header = item.data;
+                in_stream = new[header[7:0] + 2];
+                in_stream.push_back(header);
+            end
+        end
         
     endtask
+    
+    function pushData(Packet item);
+        if(item.rd_en_0 || item.rd_en_2 || item.rd_en_2)
+            out_stream.push_back(item.dout_0 || item.dout_1 || item.dout_0 );
+        in_stream.push_back(item.data)
+    endfunction
 endclass
