f386b16e1804b41a7010bf37411b6d1e840dbe44
METECH-ALT <melvinrijohnt@gmail.com>
2025-03-06 12:57:38 +0530

    v1.0

--- APB/master.sv
+++ APB/master.sv
@@ -5,6 +5,7 @@ module APB_MASTER(
     input    logic PREADY,
     input    logic PSLVERR,
     input    logic [7:0] PRDATA,
+    input    logic transfer,
     output   logic PSEL1,
     output   logic PSEL2,
     output   logic PEN,
@@ -17,4 +18,44 @@ module APB_MASTER(
     parameter ACCESS = 2'b10;
     
     reg [1:0] PS, NS;
+    reg [7:0] MEMORY [7:0];
+
+    always @(CLK) begin
+        if (!RST_N) begin
+            PS <= IDLE;
+        end
+        else begin
+            PS <= NS;
+        end
+    end
+
+    always @(*) begin
+        case (PS)
+            IDLE : begin
+                if(PSEL && !PEN) NS = SETUP;
+                else begin 
+                    NS = IDLE;
+                    PREADY = 0;
+                end
+            end
+            SETUP: begin
+                if (!PSEL || !PEN) begin
+                    NS = IDLE;
+                end
+                else begin
+                    NS = ACCESS;
+                    if(PWRITE) begin
+                        MEMORY[PADDR] <= PWDATA;
+                        PREADY <= 1;
+                        PSLVERR <= 0;
+                    end
+                    else begin
+                        PREADY <= 1;
+                        PSLVERR <= 0;
+                        PRDATA <= MEMORY[PADDR];
+                    end
+                end
+            end
+        endcase
+    end
 endmodule
